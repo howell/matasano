b0b9fb01dcfabf17f5fd4c8d9729e54e9a0dddd1
Sam Caldwell <samcaldwell@utexas.edu>
2013-09-26 20:04:15 -0500

    create xor files

---
+++ group1/xor.c
@@ -0,0 +1,14 @@
+/*
+ * xor.c
+ * Functions related to xor-ing for the Matasano crypto challenges,
+ * particularly:
+ *  1) Fixed xor
+ *  2) Single-character xor Cipher
+ *  3) Detecting single-character xor
+ *  4) Repeating-key xor cipher
+ *  5) ...
+ */
+
+#include "xor.h"
+
+
