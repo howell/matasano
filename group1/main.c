1085b2c509fc2bba7395ffc746ce4b9072396b7c
Sam Caldwell <samcaldwell@utexas.edu>
2013-09-30 22:33:43 -0500

    check all tests with asserts

--- group1/main.c
+++ group1/main.c
@@ -19,13 +19,6 @@ static void test_fixed_xor();
 static void test_break_repeat_key();
 static void test_hamming_distance();
 
-const uint8_t test_buf[] = {
-    0x49, 0x27, 0x6d, 0x20, 0x6b, 0x69, 0x6c, 0x6c, 0x69, 0x6e, 0x67,
-    0x20, 0x79, 0x6f, 0x75, 0x72, 0x20, 0x62, 0x72, 0x61, 0x69, 0x6e,
-    0x20, 0x6c, 0x69, 0x6b, 0x65, 0x20, 0x61, 0x20, 0x70, 0x6f, 0x69,
-    0x73, 0x6f, 0x6e, 0x6f, 0x75, 0x73, 0x20, 0x6d, 0x75, 0x73, 0x68,
-    0x72, 0x6f, 0x6f, 0x6d
-};
 
 int main(void)
 {
@@ -36,10 +29,6 @@ int main(void)
     test_read_base64();
     test_base_16();
     test_base64();
-    struct letter_frequencies f = { {0} };
-    calculate_letter_frequencies("abcdefghijklmnopqrstuvwxyz", &f);
-//    print_frequencies(&f);
-    printf("%.2f\n", compare_to_english(&f));
     test_break_repeat_key();
     test_hamming_distance();
     return 0;
@@ -51,21 +40,23 @@ int main(void)
  */
 static void test_print_base64()
 {
-    char *test_str = "pleasure.";
-    print_base64((uint8_t *) test_str, strlen(test_str));
-    // should print "cGxlYXN1cmUu"
-    test_str = "leasure.";
-    print_base64((uint8_t *) test_str, strlen(test_str));
-    // should print "bGVhc3VyZS4="
-    test_str = "easure.";
-    print_base64((uint8_t *) test_str, strlen(test_str));
-    // should print "ZWFzdXJlLg=="
+    char *test_str = "sure.";
+    char test_output[32] = { '\0' };
+    sprint_base64(test_output, (uint8_t *) test_str, strlen(test_str));
+    assert(strcmp(test_output, "c3VyZS4=") == 0);
     test_str = "asure.";
-    print_base64((uint8_t *) test_str, strlen(test_str));
-    // should print "YXN1cmUu"
-    test_str = "sure.";
-    print_base64((uint8_t *) test_str, strlen(test_str));
-    // should print "c3VyZS4="
+    sprint_base64(test_output, (uint8_t *) test_str, strlen(test_str));
+    assert(strcmp(test_output, "YXN1cmUu") == 0);
+    test_str = "easure.";
+    sprint_base64(test_output, (uint8_t *) test_str, strlen(test_str));
+    assert(strcmp(test_output, "ZWFzdXJlLg==") == 0);
+    test_str = "leasure.";
+    sprint_base64(test_output, (uint8_t *) test_str, strlen(test_str));
+    assert(strcmp(test_output, "bGVhc3VyZS4=") == 0);
+    test_str = "pleasure.";
+    sprint_base64(test_output, (uint8_t *) test_str, strlen(test_str));
+    assert(strcmp(test_output, "cGxlYXN1cmUu") == 0);
+    printf("Print base64 test passed!\n");
 }
 
 /*
@@ -74,8 +65,10 @@ static void test_print_base64()
 static void test_print_base16()
 {
     uint8_t hex_buf[] = { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
-    print_base16(hex_buf, sizeof hex_buf);
-    // should print out the hex alphabet
+    char test_output[32] = { '\0' };
+    sprint_base16(test_output, hex_buf, sizeof hex_buf);
+    assert(strcmp(test_output, "0123456789abcdef") == 0);
+    printf("Print base16 test passed!\n");
 }
 
 /*
@@ -86,8 +79,10 @@ static void test_read_base16()
     const char hex_str[] = "0123456789AbCDef";
     uint8_t out_buf[((sizeof hex_str) - 1) / 2]  = { 0 };
     read_base16(out_buf, hex_str, strlen(hex_str));
-    print_base16(out_buf, sizeof out_buf);
-    // should print hex alphabet
+    const uint8_t expected[sizeof out_buf] = {0x01, 0x23, 0x45, 0x67, 0x89,
+        0xAB, 0xCD, 0xEF };
+    assert(memcmp(out_buf, expected, sizeof out_buf) == 0);
+    printf("Read base16 test passed!\n");
 }
 
 static void test_base_16()
@@ -97,9 +92,8 @@ static void test_base_16()
     read_base16(out_buf, hex_str, strlen(hex_str));
     char out_str[sizeof out_buf + 1] = { '\0' };
     sprint_base16(out_str, out_buf, sizeof out_buf);
-    printf("%s\n%s\n", out_str, hex_str);
     assert(strcmp(out_str, hex_str) == 0);
-    printf("Base16 Test Passed!\n");
+    printf("Base16 round-trip test passed!\n");
 }
 
 /*
@@ -111,8 +105,14 @@ static void test_read_base64()
 //    const char base64_str[] = "SSdt";
     uint8_t out_buf[(((sizeof base64_str) - 1) * 3) / 4] = { 0 };
     read_base64(out_buf, base64_str, strlen(base64_str));
-    print_base16(out_buf, sizeof out_buf);
-    print_base64(out_buf, sizeof out_buf);
+    const uint8_t expected[sizeof out_buf] = {
+        0x49, 0x27, 0x6d, 0x20, 0x6b, 0x69, 0x6c, 0x6c, 0x69, 0x6e, 0x67,
+        0x20, 0x79, 0x6f, 0x75, 0x72, 0x20, 0x62, 0x72, 0x61, 0x69, 0x6e,
+        0x20, 0x6c, 0x69, 0x6b, 0x65, 0x20, 0x61, 0x20, 0x70, 0x6f, 0x69,
+        0x73, 0x6f, 0x6e, 0x6f, 0x75, 0x73, 0x20, 0x6d, 0x75, 0x73, 0x68,
+        0x72, 0x6f, 0x6f, 0x6d };
+    assert(memcmp(out_buf, expected, sizeof out_buf) == 0);
+    printf("Read base64 test passed!\n");
 }
 
 /*
@@ -124,33 +124,31 @@ static void test_base64()
     // string
     const char *test_str = "c3VyZS4=";
     uint8_t test_out[50] = { 0 };
-    uint32_t len = read_base64(test_out, test_str, strlen(test_str));
-    printf("%s\n", test_out);   // should print "sure."
+    size_t len = read_base64(test_out, test_str, strlen(test_str));
+    assert(memcmp(test_out, "sure.", 5) == 0);
     char out_str[50] = { '\0'};
     sprint_base64(out_str, test_out, len);
     assert(strcmp(out_str, test_str) == 0);
 
     test_str = "YXN1cmUu";
     len = read_base64(test_out, test_str, strlen(test_str));
-    printf("%s\n", test_out);   // should print "asure."
-    print_base64(test_out, len);
+    assert(memcmp(test_out, "asure.", 6) == 0);
     sprint_base64(out_str, test_out, len);
     assert(strcmp(out_str, test_str) == 0);
 
     test_str = "ZWFzdXJlLg==";
     len = read_base64(test_out, test_str, strlen(test_str));
-    printf("%s\n", test_out);   // should print "easure."
-    print_base64(test_out, len);
+    assert(memcmp(test_out, "easure.", 7) == 0);
     sprint_base64(out_str, test_out, len);
     assert(strcmp(out_str, test_str) == 0);
 
     test_str = "cGxlYXN1cmUu";
     len = read_base64(test_out, test_str, strlen(test_str));
-    printf("%s\n", test_out);   // should print "leasure."
-    print_base64(test_out, len);
+    assert(memcmp(test_out, "pleasure.", 9) == 0);
     sprint_base64(out_str, test_out, len);
     assert(strcmp(out_str, test_str) == 0);
-    printf("Base64 Test Passed!\n");
+
+    printf("Base64 round-trip test passed!\n");
 }
 
 /*
@@ -169,7 +167,6 @@ static void test_fixed_xor()
     fixed_xor(out_raw, src1_raw, src2_raw, sizeof out_raw);
     char out_str[sizeof src1];
     sprint_base16(out_str, out_raw, sizeof out_raw);
-    printf("Fixed XOR Result: %s\n", out_str);
     const char *expected = "746865206b696420646f6e277420706c6179";
     assert(strcmp(out_str, expected) == 0);
     printf("Fixed XOR Test Passed!\n");
@@ -185,16 +182,10 @@ static void test_break_repeat_key()
     uint8_t raw_cipher[(sizeof cipher_text - 1) / 2];
     read_base16(raw_cipher, cipher_text, strlen(cipher_text));
     uint8_t key = detect_repeated_key_xor(raw_cipher, sizeof raw_cipher);
-    printf("key: 0x%02x -- %c\n", key, key);
     repeated_key_xor(key, raw_cipher, raw_cipher, sizeof raw_cipher);
-    size_t i;
-    for (i = 0; i < sizeof raw_cipher; ++i)
-        printf("%c", raw_cipher[i]);
-    printf("\n");
     assert(memcmp(raw_cipher, "Cooking MC's like a pound of bacon",
                 sizeof raw_cipher) == 0);
     printf("Passed repeat key xor test!\n");
-    // Cooking MC's like a pound of bacon
 }
 
 /*
@@ -207,7 +198,6 @@ static void test_hamming_distance()
     uint32_t hamming = hamming_distance((uint8_t *) a, (uint8_t *) b,
             strlen(a));
     uint32_t expected = 37;
-    printf("Hamming distance = %d, expected %d\n", hamming, expected);
     assert(hamming == expected);
 }
 
