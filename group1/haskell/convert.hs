706d06f5a51cafb6ad508116970296d5b360164e
Sam Caldwell <samcaldwell@utexas.edu>
2013-10-22 09:02:17 -0500

    start base conversions in haskell

---
+++ group1/haskell/convert.hs
@@ -0,0 +1,32 @@
+module Convert
+( readBase16
+) where
+
+import Data.Word
+import Data.Bits
+import Data.Char
+import Data.List.Split
+import Data.List
+
+readBase16 :: String -> [Word8]
+readBase16 = map readOneBase16 $ chunk 2
+
+-- read one base 16 character
+readOneBase16 :: [Char] -> Maybe Word8
+readOneBase16 [] = Just []
+readOneBase16 [x] = base16Lookup x
+readOneBase16 [x, y] =
+readOneBase16 = combineNibbles . getNibbles
+
+base16Lookup :: Char -> Maybe Word8
+base16Lookup c = elemIndex . toLower $ c "0123456789abcdef"
+
+-- seperate the most significant and least significant nibbles of a character
+getNibbles :: Char -> (Word8, Word8)
+getNibbles c = (raw `shiftR` 4, raw .&. 4) where
+                raw = fromIntegral . ord . toLower $ c
+
+combineNibbles :: (Word8, Word8) -> Word8
+combineNibbles (x, y) = x `shiftL` 4 .|. y
+
+
