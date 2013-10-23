e6cfff0f83f10521460c9ededd183b8ad0b3e7cc
Sam Caldwell <samcaldwell@utexas.edu>
2013-10-22 21:30:34 -0500

    add read base 16

--- group1/haskell/convert.hs
+++ group1/haskell/convert.hs
@@ -8,25 +8,35 @@ import Data.Char
 import Data.List.Split
 import Data.List
 
-readBase16 :: String -> [Word8]
-readBase16 = map readOneBase16 $ chunk 2
+readBase16 :: String -> Maybe [Word8]
+readBase16 s = sequence $ map readOneBase16 $ chunksOf 2 s
+--readBase16 = map readOneBase16 $ chunk 2
 
 -- read one base 16 character
 readOneBase16 :: [Char] -> Maybe Word8
-readOneBase16 [] = Just []
+readOneBase16 [] = Nothing
 readOneBase16 [x] = base16Lookup x
-readOneBase16 [x, y] =
-readOneBase16 = combineNibbles . getNibbles
+readOneBase16 [x, y] = do
+    a <- base16Lookup x
+    b <- base16Lookup y
+    return $ a `shiftL` 4 .|. b
+readOneBase16 xs = Nothing
 
 base16Lookup :: Char -> Maybe Word8
-base16Lookup c = elemIndex . toLower $ c "0123456789abcdef"
+base16Lookup c = do
+    x <- elemIndex (toLower c) "0123456789abcdef"
+    return $ fromIntegral x
+-- These both work, no idea what the best way to do it is
+--base16Lookup c = maybe Nothing (return . fromIntegral) $ elemIndex (toLower
+                    --c) "0123456789abcdef"
+--base16Lookup c = elemIndex (toLower c) "0123456789abcdef" >>=  return . fromIntegral
 
 -- seperate the most significant and least significant nibbles of a character
-getNibbles :: Char -> (Word8, Word8)
-getNibbles c = (raw `shiftR` 4, raw .&. 4) where
-                raw = fromIntegral . ord . toLower $ c
+--getNibbles :: Char -> (Word8, Word8)
+--getNibbles c = (raw `shiftR` 4, raw .&. 4) where
+                --raw = fromIntegral . ord . toLower $ c
 
-combineNibbles :: (Word8, Word8) -> Word8
-combineNibbles (x, y) = x `shiftL` 4 .|. y
+--combineNibbles :: (Word8, Word8) -> Word8
+--combineNibbles (x, y) = x `shiftL` 4 .|. y
 
 
