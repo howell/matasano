564ea9e31361f1f26e0cd715f714af60edbd9470
Sam Caldwell <samcaldwell@utexas.edu>
2013-10-24 20:29:18 -0500

    Add another way to readOneBase16 in one line

--- group1/haskell/convert.hs
+++ group1/haskell/convert.hs
@@ -7,9 +7,10 @@ import Data.Bits
 import Data.Char
 import Data.List.Split
 import Data.List
+import Control.Monad
 
 readBase16 :: String -> Maybe [Word8]
-readBase16 s = sequence $ map readOneBase16 $ chunksOf 2 s
+readBase16 s = sequence . map readOneBase16 $ chunksOf 2 s
 --readBase16 = map readOneBase16 $ chunk 2
 
 -- read one base 16 character
@@ -23,13 +24,15 @@ readOneBase16 [x, y] = do
 readOneBase16 xs = Nothing
 
 base16Lookup :: Char -> Maybe Word8
-base16Lookup c = do
-    x <- elemIndex (toLower c) "0123456789abcdef"
-    return $ fromIntegral x
--- These both work, no idea what the best way to do it is
+base16Lookup c = liftM fromIntegral $ toLower c `elemIndex` "0123456789abcdef"
+-- These all work, no idea what the best way to do it is
+--base16Lookup c = do
+    --x <- elemIndex (toLower c) "0123456789abcdef"
+    --return $ fromIntegral x
 --base16Lookup c = maybe Nothing (return . fromIntegral) $ elemIndex (toLower
                     --c) "0123456789abcdef"
---base16Lookup c = elemIndex (toLower c) "0123456789abcdef" >>=  return . fromIntegral
+--base16Lookup c = toLower c `elemIndex` "0123456789abcdef" >>=
+--                      return . fromIntegral
 
 -- seperate the most significant and least significant nibbles of a character
 --getNibbles :: Char -> (Word8, Word8)
