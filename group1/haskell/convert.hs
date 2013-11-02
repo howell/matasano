ef36f0b13eed4bab6fe140e30ae0f7c11bd0eb75
Sam Caldwell <samcaldwell@utexas.edu>
2013-11-01 21:28:24 -0500

    add print base 64

--- group1/haskell/convert.hs
+++ group1/haskell/convert.hs
@@ -1,5 +1,8 @@
 module Convert
 ( readBase16
+, showBase16
+, readBase64
+, showBase64
 ) where
 
 import Data.Word
@@ -34,8 +37,8 @@ base16Lookup c = liftM fromIntegral $ toLower c `elemIndex` "0123456789abcdef"
 --base16Lookup c = toLower c `elemIndex` "0123456789abcdef" >>=
 --                      return . fromIntegral
 
-printBase16 :: [Word8] -> String
-printBase16 = concat . map base16ReverseLookup
+showBase16 :: [Word8] -> String
+showBase16 = concat . map base16ReverseLookup
 
 base16ReverseLookup :: Word8 -> String
 base16ReverseLookup x = [(base16Alphabet !! msn), (base16Alphabet !! lsn)] where
@@ -51,15 +54,15 @@ base64Lookup c = liftM fromIntegral $ c `elemIndex` base64Alphabet
 
 readBase64 :: String -> Maybe [Word8]
 readBase64 s = do
-    tuples <- sequence $ map readBase64Group $ tuplefy64 s
-    return . concat $ map detuplefy64 tuples
+    tuples <- sequence $ map readBase64Group $ tuplefy4 s
+    return . concat $ map detuplefy3 tuples
 
-tuplefy64 :: [Char] -> [(Char, Char, Char, Char)]
-tuplefy64 [] = []
-tuplefy64 (a:b:c:d:xs) = (a, b, c, d) : tuplefy64 xs
+tuplefy4 :: [a] -> [(a, a, a, a)]
+tuplefy4 [] = []
+tuplefy4 (a:b:c:d:xs) = (a, b, c, d) : tuplefy4 xs
 
-detuplefy64 :: (Word8, Word8, Word8) -> [Word8]
-detuplefy64 (a, b, c) = [a, b, c]
+detuplefy3 :: (a, a, a) -> [a]
+detuplefy3 (a, b, c) = [a, b, c]
 
 readBase64Group :: (Char, Char, Char, Char) -> Maybe (Word8, Word8, Word8)
 readBase64Group (a, b, c, d) = do
@@ -71,4 +74,31 @@ readBase64Group (a, b, c, d) = do
         y = (f `shiftL` 4) .|. (g `shiftR` 2)
         z = (g `shiftL` 6) .|. h
     return (x, y, z)
+
+showBase64 :: [Word8] -> String
+showBase64 xs =
+    let groups = chunksOf 3 xs
+        fullGroups = join $ map tuplefy3 $ init groups
+        lastGroup = last groups in
+    concat $ map encodeGroup64 fullGroups ++ [encodeWithPadding64 lastGroup]
+
+tuplefy3 :: [a] -> [(a, a, a)]
+tuplefy3 [] = []
+tuplefy3 (a:b:c:xs) = (a, b, c) : tuplefy3 xs
+
+encodeGroup64 :: (Word8, Word8, Word8) -> String
+encodeGroup64 (x, y, z) = [encode64 a, encode64 b, encode64 c, encode64 d] where
+    a = x `shiftR` 2
+    b = ((x .&. 0x3) `shiftL` 4) .|. (y `shiftR` 4)
+    c = ((y .&. 0xf) `shiftL` 2) .|. (z `shiftR` 6)
+    d = z .&. 0x3f
+
+encodeWithPadding64 :: [Word8] -> String
+encodeWithPadding64 [] = []
+encodeWithPadding64 [x] = take 2 (encodeGroup64 (x, 0 , 0)) ++ "=="
+encodeWithPadding64 [x, y] = take 3 (encodeGroup64 (x, y, 0)) ++ "="
+encodeWithPadding64 [x, y, z] = encodeGroup64 (x, y, z)
+
+encode64 :: Word8 -> Char
+encode64 x = base64Alphabet !! fromIntegral x
 
