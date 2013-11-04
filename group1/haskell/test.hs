37e30bced318ef3bd6669f8da75493f8c45a4955
Sam Caldwell <samcaldwell@utexas.edu>
2013-11-04 08:54:48 -0600

    start adding hunit tests

---
+++ group1/haskell/test.hs
@@ -0,0 +1,59 @@
+
+import Test.HUnit
+import Convert
+import XORCiphers
+
+import Data.Word
+import Data.Char
+
+base64Tests = TestList [TestLabel "show64Test1" show64Test1,
+                        TestLabel "show64Test2" show64Test2,
+                        TestLabel "show64Test3" show64Test3,
+                        TestLabel "show64Test4" show64Test4,
+                        TestLabel "show64Test5" show64Test5,
+                        TestLabel "read64Test1" read64Test1]
+
+base16Tests = TestList [TestLabel "show16Test1" show16Test1,
+                        TestLabel "read16Test1" read16Test1]
+
+-- helper function for using strings as test inputs
+stringToWord8 :: String -> [Word8]
+stringToWord8 = map (fromIntegral . ord)
+
+show64Test1 = TestCase (assertEqual "show64" expected actual) where
+    actual = showBase64 $ stringToWord8 "sure."
+    expected = "c3VyZS4="
+
+show64Test2 = TestCase (assertEqual "show64" expected actual) where
+    actual = showBase64 $ stringToWord8 "asure."
+    expected = "YXN1cmUu"
+
+show64Test3 = TestCase (assertEqual "show64" expected actual) where
+    actual = showBase64 $ stringToWord8 "easure."
+    expected = "ZWFzdXJlLg=="
+
+show64Test4 = TestCase (assertEqual "show64" expected actual) where
+    actual = showBase64 $ stringToWord8 "leasure."
+    expected = "bGVhc3VyZS4="
+
+show64Test5 = TestCase (assertEqual "show64" expected actual) where
+    actual = showBase64 $ stringToWord8 "pleasure."
+    expected = "cGxlYXN1cmUu"
+
+-- Matasano #1
+read64Test1 = TestCase (assertEqual "read64" expected actual) where
+    actual = readBase64 "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    expected = Just [0x49, 0x27, 0x6d, 0x20, 0x6b, 0x69, 0x6c, 0x6c, 0x69, 0x6e,
+        0x67, 0x20, 0x79, 0x6f, 0x75, 0x72, 0x20, 0x62, 0x72, 0x61, 0x69, 0x6e,
+        0x20, 0x6c, 0x69, 0x6b, 0x65, 0x20, 0x61, 0x20, 0x70, 0x6f, 0x69, 0x73,
+        0x6f, 0x6e, 0x6f, 0x75, 0x73, 0x20, 0x6d, 0x75, 0x73, 0x68, 0x72, 0x6f,
+        0x6f, 0x6d]
+
+show16Test1 = TestCase (assertEqual "show16" expected actual) where
+    actual = showBase16 $ [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef]
+    expected = "0123456789abcdef"
+
+read16Test1 = TestCase (assertEqual "read16" expected actual) where
+    actual = readBase16 "0123456789AbCDef";
+    expected = Just [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF]
+
