bebcaa5aac1b47e4793e9c1aa8699f809764afb9
Sam Caldwell <samcaldwell@utexas.edu>
2013-11-23 23:01:51 -0600

    start working on breaking xor

--- group1/haskell/xor_ciphers.hs
+++ group1/haskell/xor_ciphers.hs
@@ -3,7 +3,18 @@ module XORCiphers
 ) where
 
 import Data.Bits
+import FrequencyAnalysis
 
 fixedXOR :: Bits a => [a] -> [a] -> [a]
 fixedXOR = zipWith xor
 
+repeatKeyXOR :: Bits a => [a] -> [a] -> [a]
+repeatKeyXOR k = fixedXOR (concat $ repeat k)
+
+--findKey :: [Word8] -> Word8
+--findKey xs = maxIndex map (\x -> repeatKeyXOR (return x) xs) [0..255]
+
+breakSingleCharXORCipher :: [Word8] -> String
+breakSingleCharXORCipher xs = ""
+
+maxIndex xs = maximumBy (comparing fst) (zip xs [0..])
